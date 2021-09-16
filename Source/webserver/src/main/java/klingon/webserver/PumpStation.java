237c7c40acc7f813354d0f2c4a83692983c6480e
Morshok <78026935+Morshok@users.noreply.github.com>
2021-09-16 11:55:13 +0200

    Updated Javadoc documentation according to standards, and fixed some issues related to the Javadoc comments

--- Source/webserver/src/main/java/klingon/webserver/PumpStation.java
+++ Source/webserver/src/main/java/klingon/webserver/PumpStation.java
@@ -31,11 +31,11 @@ public class PumpStation
     /**
      * Constructor for initializing a PumpStation instance.
      *
-     * @param id        The id of the PumpStation
-     * @param address   The address of the PumpStation
-     * @param comment   A comment associated with the PumpStation, often times left empty
-     * @param latitude  The latitude component of the geographical coordinate at which the PumpStation is located
-     * @param longitude The longitude component of the geographical coordinate at which the PumpStation is located
+     * @param id        The id of this PumpStation
+     * @param address   The address of this PumpStation
+     * @param comment   A comment associated with this PumpStation, often times left empty
+     * @param latitude  The latitude component of the geographical coordinate at which this PumpStation is located
+     * @param longitude The longitude component of the geographical coordinate at which this PumpStation is located
      */
     public PumpStation(Long id, String address, String comment, double latitude, double longitude)
     {
@@ -50,73 +50,73 @@ public class PumpStation
     /**
      * Method for getting the id from a PumpStation object
      *
-     * @return Returns the id of the PumpStation
+     * @return Returns the id of this PumpStation
      */
     public Long getId() { return id; }
 
     /**
      * Method for getting the address from a PumpStation object
      *
-     * @return Returns the address of the PumpStation
+     * @return Returns the address of this PumpStation
      */
     public String getAddress() { return address; }
 
     /**
      * Method for getting the comment from a PumpStation object
      *
-     * @return Returns the comment of the PumpStation
+     * @return Returns the comment of this PumpStation
      */
     public String getComment() { return comment; }
 
     /**
      * Method for getting the latitude from a PumpStation object
      *
-     * @return Returns the latitude of the PumpStation
+     * @return Returns the latitude of this PumpStation
      */
     public double getLatitude() { return latitude; }
 
     /**
      * Method for getting the longitude from a PumpStation object
      *
-     * @return Returns the longitude of the PumpStation
+     * @return Returns the longitude of this PumpStation
      */
     public double getLongitude() { return longitude; }
 
     /**
      * Method for setting the id of a PumpStation object
      *
-     * @param id        The id of the PumpStation
+     * @param id        The id of this PumpStation
      */
     public void setId(Long id) { this.id = id; }
     /**
-     * Method for setting the address a PumpStation object
+     * Method for setting the address of a PumpStation object
      *
-     * @param address   The address of the PumpStation
+     * @param address   The address of this PumpStation
      */
     public void setAddress(String address) { this.address = address; }
     /**
      * Method for setting the comment of a PumpStation object
      *
-     * @param comment   The comment of the PumpStation
+     * @param comment   The comment of this PumpStation
      */
     public void setComment(String comment) { this.comment = comment; }
     /**
      * Method for setting the latitude of a PumpStation object
      *
-     * @param latitude  The latitude of the PumpStation
+     * @param latitude  The latitude of this PumpStation
      */
     public void setLatitude(double latitude) { this.latitude = latitude; }
     /**
      * Method for setting the longitude of a PumpStation object
      *
-     * @param longitude The longitude of the PumpStation
+     * @param longitude The longitude of this PumpStation
      */
     public void setLongitude(double longitude) { this.longitude = longitude; }
 
     /**
-     * Method for getting PumpStation as a String object
+     * Method for getting PumpStation as a String
      *
-     * @return Returns the PumpStation object represented as a String
+     * @return Returns this PumpStation object represented as a String
      */
     @Override
     public String toString()
--- Source/webserver/src/main/java/klingon/webserver/WebserverApplication.java
+++ Source/webserver/src/main/java/klingon/webserver/WebserverApplication.java
@@ -6,25 +6,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.List;
 
-/**
- * The PumpStation class is essentially a Java Bean
- * class, which is used to encapsulate a number of
- * objects into a single one. Hence this class
- * encapsulates multiple PumpStation objects into
- * a single one, namely this class (the bean).
- *
- * @author Anthon Lenander
- * @version 2021-09-15
- */
 @SpringBootApplication
-public class WebserverApplication {
-
+public class WebserverApplication
+{
 	public static void main(String[] args)
 	{
 		ConfigurableApplicationContext context = SpringApplication.run(WebserverApplication.class, args);
 		PumpStationRepository pumpStationRepository = context.getBean(PumpStationRepository.class);
+
 		List<PumpStation> allPumpStations = PumpStationAPIDataHandler.getAllPumpStations();
 		pumpStationRepository.saveAll(allPumpStations);
 	}
-
 }
