4fb726bbe32f9925017dc7917470b68d0454f57d
Phong Nguyen <apple_phong@hotmail.com>
2021-09-16 11:20:21 +0200

    Add javadocs and reformat code

--- Source/webserver/src/main/java/klingon/webserver/WebserverApplication.java
+++ Source/webserver/src/main/java/klingon/webserver/WebserverApplication.java
@@ -3,21 +3,22 @@ package klingon.webserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
 import java.util.List;
 
 @SpringBootApplication
 public class WebserverApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ConfigurableApplicationContext configurableApplicationContext =
-				SpringApplication.run(WebserverApplication.class, args);
-		BicycleStationRepository bicycleStationRepository =
-				configurableApplicationContext.getBean(BicycleStationRepository.class);
-		List<BicycleStation> bicycleStations = JsonParser.getBicycleStationData();
-		bicycleStationRepository.saveAll(bicycleStations);
+        ConfigurableApplicationContext configurableApplicationContext =
+                SpringApplication.run(WebserverApplication.class, args);
+        BicycleStationRepository bicycleStationRepository =
+                configurableApplicationContext.getBean(BicycleStationRepository.class);
+        List<BicycleStation> bicycleStations = JsonParser.getBicycleStationData();
+        bicycleStationRepository.saveAll(bicycleStations);
 
-	}
+    }
 
 
 }
