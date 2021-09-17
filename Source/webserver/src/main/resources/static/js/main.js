8de81c2480b03565ffa31aa8155f4d7d52ffda8f
Erik Kieu <erik.kieu@gmail.com>
2021-09-17 10:54:50 +0200

    fixed again

--- Source/webserver/src/main/resources/static/js/main.js
+++ Source/webserver/src/main/resources/static/js/main.js
@@ -7,3 +7,9 @@ $("button#menu_toggle").click(function(e){
 	$("nav ul").toggleClass("visible");
 	$("nav button#menu_toggle .fa").toggleClass("fa-bars fa-times");
 });
+var map = L.map('map').setView([57.690072772287735, 11.974254546462964], 16);
+L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=7Y1QmhU25CpvrabZ6trI',{
+	attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+}).addTo(map);
+var marker = L.marker([57.690072772287735, 11.974254546462964]).addTo(map);
+marker.bindPopup("<b>Chalmers Johanneberg</b><br>Campus").openPopup();
