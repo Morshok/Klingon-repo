4f4ad4038a59a3ed077e81d5c4d741b5fe8a34fa
mishaelse <seifum@student.chalmers.se>
2021-10-19 18:00:12 +0200

    Hotfix 4

--- Source/webserver/src/main/resources/static/js/main.js
+++ Source/webserver/src/main/resources/static/js/main.js
@@ -24,6 +24,10 @@ const noBikeIcon = L.icon({
     iconUrl: '/images/bicycleStationGray.png',
     iconSize: [32, 32],
 });
+const endPoint = L.icon({
+    iconUrl: '/images/locationRed.png',
+    iconSize: [32, 32],
+});
 
 $("button#weather-data-toggle").click(function () {
     $("div#weather-data").toggleClass("closed");
@@ -154,6 +158,7 @@ function loadMarker() {
             dataType: "json",
             data: {city: $("#cities-dropdown").val() },
             complete: function (response) {
+                allMarkers[markerGroup.apiPath] = []; // reset all markers for this group
                 if (response.status === 200) {
                     let data = response.responseJSON;
                     data.forEach(function (station) {
@@ -424,10 +429,11 @@ $(".navigation-select").select2({
         let availableBikesStr = item.availableBikes ? `<p>Tillg. cyklar:&nbsp; <b>${item.availableBikes}</b></p>` : "";
         let distance = item.distance;
         let distanceStr = distance > 0 ? `<p>Avstånd:&nbsp; ${formatDistanceFromMeters(distance)}</p>` : "";
+        let title = item.company ?? item.groupTitle;
 
         return $(`
             <div class="select2-result-item">
-                <label><i>${item.groupTitle}</i> - <b>${item.address}</b></label>
+                <label><i>${title}</i> - <b>${item.address}</b></label>
                 <div class="panel">
                     ${availableBikesStr}
                     ${distanceStr}
@@ -579,7 +585,7 @@ const router = L.routing.openrouteservice("5b3ce3597851110001cf6248d29230ce91e84
         "elevation": "true",
         "maneuvers": "true",
         "preference": "recommended",
-    }
+    },
 });
 
 window.routeControl = null;
@@ -591,8 +597,8 @@ function addRoute(start, end, transportationMode) {
         router: router,
         defaultErrorHandler: false,
         waypoints: [
-            L.latLng(start.latitude, start.longitude),
-            L.latLng(end.latitude, end.longitude)
+            new L.Routing.Waypoint([start.latitude, start.longitude], start.text),
+            new L.Routing.Waypoint([end.latitude, end.longitude], end.text, {icon: endPoint}),
         ],
         draggableWaypoints: false,
         addWaypoints: false,
